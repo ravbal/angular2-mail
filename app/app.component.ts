9bcf00fcb3f84692dc405932dd5610527f744287
ravali97 <ravali90@yahoo.com>
2016-10-05 11:45:45 -0700

    Created base structure for email application

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,1 +1,18 @@
+import {Component,Input} from '@angular/core';
+
+
+@Component({
+	selector:'app-component',
+	template:'<h1>Email Application</h1>',
+	stylesUrl:['app.componnet.css']
+
+
+})
+
+
+export class AppComponent{
+
+
+
+}
 
