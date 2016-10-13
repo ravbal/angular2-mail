f0f1c1ba79bed5b16747fce596bf99d442ae3941
ravali97 <ravali90@yahoo.com>
2016-10-13 13:54:56 -0700

    Displaying emails

--- app/email-detail/email-detail.component.ts
+++ app/email-detail/email-detail.component.ts
@@ -1,35 +1,37 @@
-import {Component} from '@angular/core';
-import {ActivatedRoute,Params} from '@angular/router';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Location} from '@angular/common';
 import {EmailService} from '../email.service';
 import {Email} from '../email';
 
 @Component({
-	selector:'my-email-detail',
-	templateUrl:'/app/email-detail/email-detail.component.html',
-	styleUrls:['app/email-detail/email-detail.component.css']
+	selector: 'my-email-detail',
+	templateUrl: '/app/email-detail/email-detail.component.html',
+	styleUrls: ['app/email-detail/email-detail.component.css']
 
 })
 
-export class EmailDetailComponent{
+export class EmailDetailComponent implements OnInit {
 
-	email:Email;
+	@Input
+	email: Email;
+
 	constructor(private emailService: EmailService,
-				private route: ActivatedRoute
-				) {
+				private route: ActivatedRoute,
+				private location: Location) {
 	}
-
 
 	ngOnInit(): void {
-
-		this.route.params.forEach((params: Params)=> {
-
+		this.route.params.forEach((params: Params) => {
 			let id = +params['id'];
 			this.emailService.getEmail(id)
-				.then(email=>this.email = email);
+				.then(email => this.email = email);
 		});
-
 	}
 
+	goBack(): void {
+		this.location.back();
+	}
 
 
 }
--- app/mock-emails.ts
+++ app/mock-emails.ts
@@ -1,15 +1,55 @@
 import {Email} from './email';
 
-export const EMAILS:Email[]=[
-	{id:1,subject:"hello"},
-	{id:2,subject:"hi"},
-	{id:3,subject:"how are you"},
-	{id:4,subject:"im good"},
-	{id:5,subject:"wassup"},
-	{id:6,subject:"hello"},
-	{id:7,subject:"hello2"},
-	{id:8,subject:"hello3"},
-	{id:9,subject:"hello4"},
-	{id:10,subject:"hello5"}
+export const EMAILS: Email[] = [
+	{
+		id: 1,
+		subject: "Lorem Ipsum",
+		message: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived"
+	},
+	{
+		id: 2,
+		subject: "Coupons",
+		message: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old"
+	},
+	{
+		id: 3,
+		subject: "Fossil Sale",
+		message: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable"
+	},
+	{
+		id: 4,
+		subject: "Coach sale",
+		message: " Coach sale"
+	},
+	{
+		id: 5,
+		subject: "Guess coupons",
+		message: " Guess sale"
+	},
+	{
+		id: 6,
+		subject: "Mk 40% off",
+		message: " Mk 40% off"
+	},
+	{
+		id: 7,
+		subject: "Macys Vip Sale",
+		message: " Keep watching our website"
+	},
+	{
+		id: 8,
+		subject: "Nordstorm Deals",
+		message: " Anniversary sale "
+	},
+	{
+		id: 9,
+		subject: "JC Penny 25% off",
+		message: "Buy 1 get 1 Deal"
+	},
+	{
+		id: 10,
+		subject: "Payless coupons",
+		message: " Buy 2 Get 1 free"
+	}
 
 ];
